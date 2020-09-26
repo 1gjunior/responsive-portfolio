c40eb1d7b0c1d6f52e08830041c002612e5a1a16
Gilberto Junior <junior.siilva182@gmail.com>
2020-09-26 01:16:32 -0300

    added new function

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,6 +16,13 @@ showMenu('nav-toggle', 'nav-menu')
 const navLink = document.querySelectorAll('.nav__link')
 
 function linkAction() {
+    // Link Ativo
     navLink.forEach(n => n.classList.remove('active'))
     this.classList.add('active')
+
+    // Remove menu mobile
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show')
 }
+
+navLink.forEach(n => n.addEventListener('click', linkAction))
